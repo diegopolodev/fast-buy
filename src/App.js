74350a722e2df54b2e7281af6df36e5f62406cf0
Digpolo <digpolo@gmail.com>
2023-02-27 18:37:14 -0500

    third commit

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import {
   RiAddLine,
   RiPieChartLine,
   RiCloseLine,
-  RiArrowDownSLine, } from "react-icons/ri";
+  RiArrowDownSLine, 
+  RiDeleteBin6Line} from "react-icons/ri";
   
 import Sidebar from './components/shared/Sidebar'
-
 
 
 function App() {
@@ -47,9 +47,71 @@ function App() {
           {showMenu ? <RiCloseLine /> : <RiMenu3Fill />}
         </button>
       </nav>
-      <main className="lg:pl-32 lg:pr-96 pb-20">
+      <main className="lg:pl-32 grid-cols-1 lg:grid-cols-8 p-4 pb-20">
         <div>Hola1</div>
-        <div>Hola 2</div>
+
+
+        <div className="lg:col-span-2 fixed lg:static right-0 top-0 bg-[#1F1D2B] w-full h-full">
+          {/*Orders*/}
+          <div className="relative pt-16 text-gray-300 p-8">
+            <RiCloseLine className="absolute left-4 top-4 p-3 box-content text-gray-300 bg-[#262837] rounded-full"/> 
+            <h1 className="text-2xl my-4">Orders</h1>
+           {/*Pills*/}
+            <div className="flex items-center gap-4 flex-wrap mb-8">
+              <button className="bg-[#ec7c6a] text-white py-2 px-4 rounded-xl">Dine in</button>
+              <button className=" text-[#ec7c6a] py-2 px-4 rounded-xl border border-gray-500">To Go</button>
+              <button className=" text-[#ec7c6a] py-2 px-4 rounded-xl border border-gray-500">Delivery</button>
+            </div>
+            {/*Car*/}
+            <div>
+              <div className="grid grid-cols-6 mb-4 p-4">
+                <h5 className="col-span-4">Item</h5>
+                <h5 >Qty</h5>
+                <h5>Price</h5>
+              </div>
+            {/*Product*/}
+            <div className="bg-[#262837] p-4 rounded-xl">
+              <div className="grid grid-cols-6 mb-4">
+                {/*Product description*/}
+                <div className="col-span-4 flex items-center gap-2">
+                  <img src="zapatos1.jgp" className="w-20"/>
+                  <div>
+                   <h5 className="text-sm">Spaicy ...</h5>
+                   <p className="text-xs text-gray-500">$2.29</p>
+                  </div>
+                </div>
+                {/*Qty*/}
+                <div className="text-center">
+                  <span>2</span>
+                </div>
+                 {/*Price*/}
+                 <div>
+                  <span>$4.58</span>
+                 </div>
+                 {/*Note*/}
+                 <div className="grid grid-cols-6 items-center ">
+                  <form className="col-span-5"> 
+                    <input
+                     type='text' 
+                     className="bg-[#1F1D2B] py-2 px-4 rounded-lg outline-none"
+                     placeholder="Note..."/>
+                  </form>
+                  <div>
+                    <button className="border border-red-500 p-2 rounded-lg">
+                      <RiDeleteBin6Line className="text-red-500"/>
+                    </button>
+                  </div>
+
+                 </div>
+              </div>
+
+            </div>
+            </div>
+
+          </div>
+          
+
+        </div>
       </main>
     </div>
   );
