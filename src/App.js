c19bb9bd96a7579c2a2c0cad7e66d73088d1bd47
Digpolo <digpolo@gmail.com>
2023-02-27 15:47:04 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
 
+import { useState } from "react";
+
+import { 
+  RiMenu3Fill,
+  RiUser3Line,
+  RiAddLine,
+  RiPieChartLine,
+  RiCloseLine,
+  RiArrowDownSLine, } from "react-icons/ri";
+  
+import Sidebar from './components/shared/Sidebar'
+
 
 
 function App() {
+
+  const [showMenu, setShowMenu] = useState(false);
+  const [showOrder, setShowOrder] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+    setShowOrder(false);
+  };
+
+  const toggleOrders = () => {
+    setShowOrder(!showOrder);
+    setShowMenu(false);
+  };
+
+
   return (
-    <div className="bg-red-400">
-      Hola
-      
+    <div className="bg-[#262837] w-full min-h-screen">
+      <Sidebar showMenu={showMenu}/>
+      {/*Menu movil*/}
+      <nav className='bg-[#1F1D2B] lg:hidden fixed w-full bottom-0 left-0 text-2xl text-gray-400 p-4 flex items-center justify-between rounded-tl-xl rounded-tr-xl'>
+        <button>
+          <RiUser3Line/>
+          </button>   
+        <button className="p-2">
+          <RiAddLine />
+        </button>
+        <button className="p-2">
+          <RiPieChartLine />
+        </button>
+        <button onClick={toggleMenu} className="text-white p-2">
+          {showMenu ? <RiCloseLine /> : <RiMenu3Fill />}
+        </button>
+      </nav>
+      <main className="lg:pl-32 lg:pr-96 pb-20">
+        <div>Hola1</div>
+        <div>Hola 2</div>
+      </main>
     </div>
   );
 }
