a1b9db09de932e392097f81a8743100534ba5cfb
Digpolo <digpolo@gmail.com>
2023-02-28 14:40:41 -0500

    seventh commit

--- src/App.js
+++ src/App.js
@@ -8,12 +8,14 @@ import {
   RiPieChartLine,
   RiCloseLine,
   RiArrowDownSLine, 
-  RiDeleteBin6Line} from "react-icons/ri";
+  } from "react-icons/ri";
+
+   /*Components*/
+  import Sidebar from './components/shared/Sidebar';
+  import Car from "./components/shared/Car";
+  import Header from "./components/shared/Header";
   import Card from "./components/shared/Card";
   
-import Sidebar from './components/shared/Sidebar';
-
-
 function App() {
   const [showMenu, setShowMenu] = useState(false);
   const [showOrder, setShowOrder] = useState(false);
@@ -34,7 +36,7 @@ function App() {
   return (
     <div className="bg-[#262837] w-full min-h-screen">
       <Sidebar showMenu={showMenu}/>
-      <Card/>
+      <Car showOrder={showOrder} setShowOrder={setShowOrder} />
       {/*Menu movil*/}
       <nav className='bg-[#1F1D2B] lg:hidden fixed w-full bottom-0 left-0 text-2xl text-gray-400 p-4 flex items-center justify-between rounded-tl-xl rounded-tr-xl'>
         <button>
@@ -50,11 +52,91 @@ function App() {
           {showMenu ? <RiCloseLine /> : <RiMenu3Fill />}
         </button>
       </nav>
-      <main className="lg:pl-32 pb-20">
-        <div>Hola1</div>
-
-    
-       
+      <main className="lg:pl-32 lg:pr-96 pb-20">
+        <div className="md:p-8 p-4">
+          {/* Header */}
+          <Header />
+          {/* Title content */}
+          <div className="flex items-center justify-between mb-16">
+            <h2 className="text-xl text-gray-300">Choose Dishes</h2>
+            <button className="flex items-center gap-4 text-gray-300 bg-[#1F1D2B] py-2 px-4 rounded-lg">
+              <RiArrowDownSLine /> Dine in
+            </button>
+          </div>
+          {/* Content */}
+          <div className="p-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16">
+            {/* Card */}
+            <Card
+              img="dish1.png"
+              description="Speacy seasoned seafood nodles"
+              price="4.29"
+              inventory="20"
+            />
+            {/* Card */}
+            <Card
+              img="dish2.png"
+              description="Speacy seasoned seafood nodles"
+              price="2.29"
+              inventory="20"
+            />
+            {/* Card */}
+            <Card
+              img="dish3.png"
+              description="Speacy seasoned seafood nodles"
+              price="2.29"
+              inventory="20"
+            />
+            {/* Card */}
+            <Card
+              img="dish1.png"
+              description="Speacy seasoned seafood nodles"
+              price="2.29"
+              inventory="20"
+            />
+            {/* Card */}
+            <Card
+              img="dish2.png"
+              description="Speacy seasoned seafood nodles"
+              price="2.29"
+              inventory="20"
+            />
+            {/* Card */}
+            <Card
+              img="dish3.png"
+              description="Speacy seasoned seafood nodles"
+              price="2.29"
+              inventory="20"
+            />
+            {/* Card */}
+            <Card
+              img="dish1.png"
+              description="Speacy seasoned seafood nodles"
+              price="2.29"
+              inventory="20"
+            />
+            {/* Card */}
+            <Card
+              img="dish2.png"
+              description="Speacy seasoned seafood nodles"
+              price="2.29"
+              inventory="20"
+            />
+            {/* Card */}
+            <Card
+              img="dish3.png"
+              description="Speacy seasoned seafood nodles"
+              price="2.29"
+              inventory="20"
+            />
+            {/* Card */}
+            <Card
+              img="dish1.png"
+              description="Speacy seasoned seafood nodles"
+              price="2.29"
+              inventory="20"
+            />
+          </div>
+        </div>
       </main>
     </div>
   );
